4783ecc3696c8da18e543e05d286d28f5e4bcae0
mug1wara26 <aloysiusgsq@gmail.com>
2023-01-05 08:34:23 +0800

    github actions push

--- assignment1/vue.config.js
+++ assignment1/vue.config.js
@@ -1,6 +1,9 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
+import { defineConfig } from '@vue/cli-service'
+defineConfig({
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  publicPath: process.env.NODE_ENV === 'production'
+      ? '/reponame'
+      : '/'
 })
