c0151afa304a2da80e9e9544885425fd6c371654
mug1wara26 <aloysiusgsq@gmail.com>
2023-01-06 18:39:13 +0800

    github pages 404 workaround

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -10,6 +10,9 @@ import fs from "fs";
         // await execa("yarn", ["build"]);
         // Understand if it's dist or build folder
         const folderName = fs.existsSync("dist") ? "dist" : "build";
+        await execa("cd", [folderName]);
+        await execa("cp", ["index.html", "404.html"]);
+        await execa("cd", ["../"]);
         await execa("git", ["--work-tree", folderName, "add", "--all"]);
         await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
         console.log("Pushing to gh-pages...");
