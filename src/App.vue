67363f0756721453b3cbb995827824204cb596dc
mug1wara26 <aloysiusgsq@gmail.com>
2023-01-16 20:05:17 +0800

    use cookies to remember dark mode

--- src/App.vue
+++ src/App.vue
@@ -105,6 +105,7 @@
 import Vue from "vue"
 import RegisterDialog from "@/components/RegisterDialog.vue";
 import LoginDialog from "@/components/LoginDialog.vue";
+import {setCookie} from "typescript-cookie";
 
 export default Vue.extend({
   name: 'App',
@@ -167,6 +168,7 @@ export default Vue.extend({
   methods: {
     toggleDark() {
       this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+      setCookie("dark", this.$vuetify.theme.dark)
     }
   },
   mounted() {
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,8 +1,11 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
+import {getCookie} from "typescript-cookie";
 
 Vue.use(Vuetify);
 
+const dark_cookie = getCookie("dark")
+const dark = (!(dark_cookie !== undefined && dark_cookie === "false"))
 export default new Vuetify({
-    theme: { dark: true },
+    theme: { dark: dark },
 });
