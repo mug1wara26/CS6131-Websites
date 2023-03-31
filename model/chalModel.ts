cda2900e9b74c9166a152975d3c01944bc83c244
mug1wara26 <42673064+mug1wara26@users.noreply.github.com>
2023-03-31 15:30:44 +0000

    Update backend to output generated at 803cb25

--- model/chalModel.ts
+++ model/chalModel.ts
@@ -19,6 +19,35 @@ WHERE BINARY ctfid = ?
     )
 }
 
+export const findOneByUser = (ctfid: string, name: string, username: string, callback: Function) => {
+    const queryString = `
+SELECT chal.name, chal.ctfid, chal.difficulty, chal.category, chal.points
+FROM challenge chal, ctf, team t, member m
+WHERE chal.ctfid = ?
+and binary chal.name = ?
+and binary m.username = ?
+and ctf.id = chal.ctfid
+and ctf.teamCreator = t.name 
+and m.teamName = t.name
+UNION
+SELECT chal.name, chal.ctfid, chal.difficulty, chal.category, chal.points
+FROM challenge chal, competitor comp
+WHERE chal.ctfid = comp.ctfid
+and binary chal.ctfid = ?
+and binary chal.name = ?
+and comp.competitorName = ?
+    `
+
+    db.query(
+        queryString,
+        [ctfid, name, username, ctfid, name, username],
+        (err, result) => {
+            if (err) callback(err)
+            else callback(null, (<RowDataPacket> result)[0])
+        }
+    )
+}
+
 export const create = (chal: Challenge, callback: Function) => {
     const queryString = `
 INSERT INTO challenge VALUES
